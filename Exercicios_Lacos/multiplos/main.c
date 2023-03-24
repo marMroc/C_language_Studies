0d9bb264bdf06645524320c65bace74296bef7a8
Marcelo Rocha <75541524+marMroc@users.noreply.github.com>
2023-03-24 16:24:41 -0300

    Mais exercícios
    Adicionando mais exercícios pra lista

---
+++ Exercicios_Lacos/multiplos/main.c
@@ -0,0 +1,21 @@
+/* Faça um programa que determine o mostre os cinco primeiros 
+múltiplos de 3, conside-rando números maiores que 0. */
+
+#include <stdio.h>
+
+int main() {
+  int i, count = 0;
+
+  printf("Os cinco primeiros múltiplos de 3 são:\n");
+
+  for(i = 1; count < 5; i++) {
+    if(i % 3 == 0) {
+      printf("%d ", i);
+      count++;
+    }
+  }
+
+  printf("\n");
+
+  return 0;
+}
